bca2ef51f93d6ec7e4e549c0d5bce6b42d2959c6
michael hohl <mhohl@rocketmail.com>
2018-03-19 21:38:41 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,8 +6,8 @@ def summon_captain_planet(array)
   array.map {|x| x.capitalize + "!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  array.find {|x| x.length > 4}? false : true
 end
 
 def find_the_cheese# code an argument here
