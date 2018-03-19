b039e74565f9de10804b0c31e4b0a20582c19f85
michael hohl <mhohl@rocketmail.com>
2018-03-19 21:40:57 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet(array)
 end
 
 def long_planeteer_calls(array)
-  array.find {|x| x.length > 4}? false : true
+  array.find {|x| x.length > 4} == nil ? false : true
 end
 
 def find_the_cheese# code an argument here
